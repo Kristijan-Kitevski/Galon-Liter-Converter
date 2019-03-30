ad0100e677b2b42b86a83bd45bede1ad3a0b9fde
MKFeniks <44569532+MKFeniks@users.noreply.github.com>
2019-03-30 11:50:55 +0100

    Add files via upload

---
+++ ExtentionMetods/ExtensionMetods/Converter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMetods
+{
+    public static class Converter
+    {
+        public static double GalonToLiter( string G)
+        {
+            double Galon = double.Parse( G);
+            double Liter = Galon * 3.185;
+            return Liter;
+        }
+
+        public static double LiterToGalion(string L)
+        {
+            double Liter = double.Parse(L);
+            double Galon = Liter / 3.185;
+            return Galon;
+        }
+    }
+}
---
+++ ExtentionMetods/ExtensionMetods/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMetods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            bool runing = true;
+
+            while (runing)
+            {
+                   Console.WriteLine("-------------------------------------");
+                   Console.WriteLine("Select The convertor direction");
+                   Console.WriteLine("1 Galons to liters");
+                   Console.WriteLine("2 Liters to galon");
+                   Console.WriteLine("3 Exit Converter");
+                   Console.WriteLine("-------------------------------------");
+
+                   string selection = Console.ReadLine();
+
+                    switch (selection)
+                    {
+                        case "1":
+                            Console.WriteLine("Enter galons to convert");
+                            double liters = Converter.GalonToLiter(Console.ReadLine());
+                            Console.WriteLine($"{liters} Liters");
+                            break;
+                        case "2":
+                            Console.WriteLine("Enter Liters to convert");
+                            double galons = Converter.LiterToGalion(Console.ReadLine());
+                            Console.WriteLine($"{galons} galons");
+                            break;
+                        case "3":
+                            Console.WriteLine("Press enter to exit");
+                               runing = false;
+                            break;
+                    }
+               
+            }
+            Console.Read();
+        }
+
+    }
+}
